7318474c9d8123a4fd32a69bdd5d3acd855552c5
christy761 <118560675+christy761@users.noreply.github.com>
2023-09-06 21:37:17 +0200

    added link to the open-source code and SheCodes profile

--- src/Weather.js
+++ src/Weather.js
@@ -13,58 +13,77 @@ export default function Weather() {
   };
 
   return (
-    <div className="Weather">
-      <h1>Weather App</h1>
-      <h2>Current weather & 5-day forecast</h2>
-      <form className="search-form">
-        <div className="row">
-          <div className="col-6">
-            <input
-              type="search"
-              placeholder="Enter a city name"
-              autoFocus="on"
-              autoComplete="off"
-              className="form-control shadow-sm"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search City"
-              className="form-control btn btn-success shadow-sm w-100 search-city-button"
-            />
-          </div>
-          <div className="col-3">
-            <button className="btn btn-success w-100 current-button">
-              Current
-            </button>
-          </div>
-        </div>
-      </form>
-      <div className="col-3">
-        <h3>{weatherData.city}</h3>
-      </div>
-      <ul>
-        <li>Last updated: {weatherData.date} </li>
-        <li>{weatherData.description}</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <div className="d-flex current-temp">
-            <img src={weatherData.imgUrl} alt={weatherData.description} />
-            <div>
-              <span className="temperature">{weatherData.temperature} </span>
-              <span className="units">°C</span>
+    <div className="Weather-App">
+      <div className="Weather">
+        <h1>Weather App</h1>
+        <h2>Current weather & 5-day forecast</h2>
+        <form className="search-form">
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Enter a city name"
+                autoFocus="on"
+                autoComplete="off"
+                className="form-control shadow-sm"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search City"
+                className="form-control btn btn-success shadow-sm w-100 search-city-button"
+              />
+            </div>
+            <div className="col-3">
+              <button className="btn btn-success w-100 current-button">
+                Current
+              </button>
             </div>
           </div>
+        </form>
+        <div className="col-3">
+          <h3>{weatherData.city}</h3>
         </div>
-        <div className="col-6">
-          <ul className="condition-information">
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
-          </ul>
+        <ul>
+          <li>Last updated: {weatherData.date} </li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <div className="d-flex current-temp">
+              <img src={weatherData.imgUrl} alt={weatherData.description} />
+              <div>
+                <span className="temperature">{weatherData.temperature} </span>
+                <span className="units">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul className="condition-information">
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
+      <small>
+        <a
+          href="https://github.com/christy761/weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        , by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/21843-christy-ramkisoen"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Christy Ramkisoen
+        </a>
+      </small>
     </div>
   );
 }
