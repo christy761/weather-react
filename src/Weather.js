4f117b95535f05171f1b2d78dc05ca9ad4fd8d1e
christy761 <118560675+christy761@users.noreply.github.com>
2023-09-11 16:21:01 +0200

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,70 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "New York",
-    temperature: 19,
-    date: "Tuesday 10:00",
-    description: "Cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    humidity: 80,
-    wind: 10,
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: "Wednesday 07:00",
+      description: response.data.condition.description,
+      iconUrl: response.data.condition.icon_url,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
+  }
 
-  return (
-    <div className="Weather">
-      <h1>Weather App</h1>
-      <h2>Current weather & 5-day forecast</h2>
-      <form className="search-form">
-        <div className="row">
-          <div className="col-6">
-            <input
-              type="search"
-              placeholder="Enter a city name"
-              autoFocus="on"
-              autoComplete="off"
-              className="form-control shadow-sm"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="form-control btn btn-success shadow-sm w-100 search-city-button"
-            />
-          </div>
-          <div className="col-3">
-            <button className="btn btn-success w-100 current-button">
-              Current
-            </button>
-          </div>
-        </div>
-      </form>
-      <div className="col-3">
-        <h3>{weatherData.city}</h3>
-      </div>
-      <ul>
-        <li>Last updated: {weatherData.date} </li>
-        <li>{weatherData.description}</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <div className="d-flex current-temp">
-            <img src={weatherData.imgUrl} alt={weatherData.description} />
-            <div>
-              <span className="temperature">{weatherData.temperature} </span>
-              <span className="units">°C</span>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <h1>Weather App</h1>
+        <h2>Current weather & 5-day forecast</h2>
+        <form className="search-form">
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Enter a city name"
+                autoFocus="on"
+                autoComplete="off"
+                className="form-control shadow-sm"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="form-control btn btn-success shadow-sm w-100 search-city-button"
+              />
+            </div>
+            <div className="col-3">
+              <button className="btn btn-success w-100 current-button">
+                Current
+              </button>
             </div>
           </div>
+        </form>
+        <div className="col-3">
+          <h3>{weatherData.city}</h3>
         </div>
-        <div className="col-6">
-          <ul className="condition-information">
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
-          </ul>
+        <ul>
+          <li>Last updated: {weatherData.date} </li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <div className="d-flex current-temp">
+              <img src={weatherData.iconUrl} alt={weatherData.description} />
+              <div>
+                <span className="temperature">
+                  {Math.round(weatherData.temperature)}
+                </span>
+                <span className="units">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul className="condition-information">
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "6ea2dfaoeac7fte3c92600dba4113151";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
