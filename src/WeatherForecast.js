d59ac26a96bbdacce4aa53195fdad8c67a84afdf
christy761 <118560675+christy761@users.noreply.github.com>
2023-09-24 00:16:38 +0200

    Created forecast layout and started to implement the API

---
+++ src/WeatherForecast.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "6ea2dfaoeac7fte3c92600dba4113151";
+  let longitude = 40.7;
+  let latitude = 74;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <img src={props.data.condition.icon_url} alt="weather-icon" />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
